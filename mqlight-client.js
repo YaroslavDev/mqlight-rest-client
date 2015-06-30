505fff737ff61a693e86f00d0e2fe85f2ddac646
Iaroslav Ciupin <Iaroslav.Ciupin@endava.com>
2015-06-30 23:49:59 +0300

    Refactoring part II

--- mqlight-client.js
+++ mqlight-client.js
@@ -1,6 +1,7 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
+
+var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 var mqlight = require('mqlight');
@@ -9,29 +10,8 @@ var opts = {
 	user: 'admin',
 	password: 'password'
 };
+
 app.post('/events', function(req, res) {
-	function respond(data, delivery) {
-		res.send(data);
-		clients.forEach(function(client) {
-			client.stop();
-		});
-	}
-	function listen(topic) {
-		var client = mqlight.createClient(opts);
-		client.subscribe(topic);
-		console.log("Listening to " + topic);
-		client.on('message', respond);
-		return client;
-	}
-	function addReplyTopic(topic) {
-		if (topic != undefined) replyTopics.push(topic);
-	}
-	function send(topic, body) {
-		var client = mqlight.createClient(opts);
-		client.send(topic, body);
-		console.log("Sending " + body + " to topic " + topic);
-		client.stop();
-	}
 	res.set('Content-Type', 'application/json');
 	var outTopic = req.headers.out;
 	if (outTopic == undefined) {
@@ -39,17 +19,54 @@ app.post('/events', function(req, res) {
 		return;
 	}
 	var replyTopics = [];
-	addReplyTopic(req.headers.succeeded);
-	addReplyTopic(req.headers.failed);
+	addReplyTopic(replyTopics, req.headers.succeeded);
+	addReplyTopic(replyTopics, req.headers.failed);
 
 	if (replyTopics.length == 0) {
 		res.send({status: "Success: OK"});
 	} else {
-		var clients = replyTopics.map(listen);
+		var clients = [];
+		var callback = function(data, delivery) {
+			res.send(data);
+			stopClients(clients);
+		}
+		replyTopics.forEach(function(topic) {
+			var client = listen(topic, callback)
+			clients.push(client);
+		});
 	}
 
 	var body = JSON.stringify(req.body);
-	send(outTopic, body);
+	sendMessage(outTopic, body);
 });
 
 var server = app.listen(3000, function() {});
+
+function addReplyTopic(replyTopics, topic) {
+	if (topic != undefined) replyTopics.push(topic);
+}
+
+function sendMessage(topic, body) {
+	var client = mqlight.createClient(opts);
+	client.send(topic, body);
+	console.log("Sending " + body + " to topic " + topic);
+	console.log("Client stopping after sending message " + client);
+	client.stop();
+}
+
+function listen(topic, callback) {
+	var client = mqlight.createClient(opts);
+	client.subscribe(topic);
+	console.log("Listening to " + topic);
+	client.on('message', callback);
+	return client;
+}
+
+function stopClients(clients) {
+	clients.forEach(function(client) {
+		if (client != undefined) {
+			console.log("Client stopping after reply " + client);
+			client.stop();
+		}
+	});
+}
