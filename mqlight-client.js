e3128f7f2c06c57790496f83860dec1fcf7912e6
Iaroslav Ciupin <Iaroslav.Ciupin@endava.com>
2015-06-30 23:10:43 +0300

    Refactoring

--- mqlight-client.js
+++ mqlight-client.js
@@ -16,33 +16,40 @@ app.post('/events', function(req, res) {
 			client.stop();
 		});
 	}
+	function listen(topic) {
+		var client = mqlight.createClient(opts);
+		client.subscribe(topic);
+		console.log("Listening to " + topic);
+		client.on('message', respond);
+		return client;
+	}
+	function addReplyTopic(topic) {
+		if (topic != undefined) replyTopics.push(topic);
+	}
+	function send(topic, body) {
+		var client = mqlight.createClient(opts);
+		client.send(topic, body);
+		console.log("Sending " + body + " to topic " + topic);
+		client.stop();
+	}
 	res.set('Content-Type', 'application/json');
 	var outTopic = req.headers.out;
 	if (outTopic == undefined) {
-		var body = {status: "Error: Specify out topic"};
-		res.send(body);
+		res.send({status: "Error: Specify out topic"});
 		return;
 	}
 	var replyTopics = [];
-	var succeeded = req.headers.succeeded;
-	if (succeeded != undefined) replyTopics.append(succeeded);
-	var failed = req.headers.failed;
-	if (failed != undefined) replyTopics.append(failed);
-	var body = JSON.stringify(req.body);
+	addReplyTopic(req.headers.succeeded);
+	addReplyTopic(req.headers.failed);
 
 	if (replyTopics.length == 0) {
-		var body = {status: "Success: OK"};
-		res.send(body);
+		res.send({status: "Success: OK"});
 	} else {
-		var clients = replyTopics.map(function(topic) {
-			var client = mqlight.createClient(opts);
-			client.subscribe(topic);
-			client.on('message', respond);
-			return client;
-		});
+		var clients = replyTopics.map(listen);
 	}
 
-	succeededClient.send(outTopic, body);
+	var body = JSON.stringify(req.body);
+	send(outTopic, body);
 });
 
 var server = app.listen(3000, function() {});
